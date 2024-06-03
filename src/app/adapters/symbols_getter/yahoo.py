88e3f373f894e2e473fcdf23c2dc86b1dad6fc57
wozrer <wozrer@proton.me>
2024-06-03 20:22:11 +0300

    Refactor

--- src/app/adapters/symbols_getter/yahoo.py
+++ src/app/adapters/symbols_getter/yahoo.py
@@ -1,5 +1,4 @@
-from yahoo_finance_async import OHLC
-from yahoo_finance_async.api import APIError
+import aiohttp
 
 from app.logic.abstract import SymbolsGetter
 from app.logic.exceptions import UnfoundSymbolError
@@ -8,6 +7,18 @@ from app.logic.exceptions import UnfoundSymbolError
 class YahooSymbolsGetter(SymbolsGetter):
     async def get_price(self, symbol: str) -> float:
         try:
-            return (await OHLC.fetch(symbol))["candles"][-1]["open"]
-        except APIError:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(
+                    f"https://query1.finance.yahoo.com/v8/finance/chart/{symbol}",
+                    params={
+                        "events": "history",
+                        "interval": "5m",
+                        "history": "1d",
+                    },
+                    ssl=False,
+                ) as resp:
+                    return (await resp.json())["chart"]["result"][0][
+                        "indicators"
+                    ]["quote"][-1]["close"][0]
+        except TypeError:
             raise UnfoundSymbolError()
