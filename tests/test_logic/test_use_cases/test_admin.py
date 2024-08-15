ceaa6e770eed04a6f6ce8b393a532db1f7595f71
wozrer <wozrer@proton.me>
2024-08-15 05:17:26 +0300

    Refactor test

--- tests/test_logic/test_use_cases/test_admin.py
+++ tests/test_logic/test_use_cases/test_admin.py
@@ -1,55 +1,51 @@
+from unittest.mock import AsyncMock
+from datetime import datetime
+
 import pytest
 
-from app.logic.abstract.storages.users import UsersOneSelector
 from app.logic.models import User, Role
 from app.logic.exceptions import PermissionDenied
 from app.logic.use_cases.admin import check_permissions
-from app.utils.funcs import get_current_time
-
-
-class MockUsersOneSelector(UsersOneSelector):
-    def __init__(self, data: list[User]) -> None:
-        self.data = data
-
-    async def select_one_by_id(self, user_id: int) -> User:
-        return list(filter(lambda x: x.id == user_id, self.data))[0]
-
-    async def select_one_by_username(self, username: str) -> User:
-        return list(filter(lambda x: x.username == username, self.data))[0]
 
 
 async def test_check_permissions_for_owner() -> None:
-    user_id = 100
-    await check_permissions(
-        MockUsersOneSelector(
-            [
-                User(
-                    id=user_id,
-                    username="abc",
-                    password="def",
-                    role=Role.OWNER,
-                    register_at=get_current_time(),
-                )
-            ]
-        ),
-        user_id,
-    )
+    USER_ID = 100
+
+    async def side_effect(user_id: int) -> User:
+        if user_id != USER_ID:
+            raise AssertionError()
+        return User(
+            id=USER_ID,
+            username="abc",
+            password="def",
+            role=Role.OWNER,
+            register_at=datetime.now(),
+        )
+
+    users_selector = AsyncMock()
+    users_selector.select_one_by_id.side_effect = side_effect
+
+    await check_permissions(users_selector, USER_ID)
+    users_selector.select_one_by_id.assert_awaited_once()
 
 
 async def test_check_permissions_for_user() -> None:
-    user_id = 100
-    with pytest.raises(PermissionDenied):
-        await check_permissions(
-            MockUsersOneSelector(
-                [
-                    User(
-                        id=user_id,
-                        username="usr",
-                        password="1234",
-                        role=Role.USER,
-                        register_at=get_current_time(),
-                    )
-                ]
-            ),
-            user_id,
+    USER_ID = 553
+
+    async def side_effect(user_id: int) -> User:
+        if user_id != USER_ID:
+            raise AssertionError()
+        return User(
+            id=USER_ID,
+            username="usr",
+            password="1234",
+            role=Role.USER,
+            register_at=datetime.now(),
         )
+
+    users_selector = AsyncMock()
+    users_selector.select_one_by_id.side_effect = side_effect
+
+    with pytest.raises(PermissionDenied):
+        await check_permissions(users_selector, USER_ID)
+    users_selector.select_one_by_id.assert_awaited_once()
