508fa6b1e5dc2260eb58e17380abe7c36c7ec60c
wozrer <wozrer@proton.me>
2024-08-17 12:09:39 +0300

    Add locks for cache currency getter

--- src/app/adapters/currency_getter/memory_cache.py
+++ src/app/adapters/currency_getter/memory_cache.py
@@ -1,3 +1,5 @@
+import asyncio
+from collections import defaultdict
 from datetime import timedelta, datetime
 from dataclasses import dataclass
 
@@ -16,6 +18,7 @@ class CachedCurrencyPrice:
 @dataclass
 class MemoryCurrencyGetter:
     price: dict[str, CachedCurrencyPrice]
+    locks: dict[str, asyncio.Lock]
 
 
 class MemoryCacheCurrencyGetter(CurrencyGetter):
@@ -31,17 +34,18 @@ class MemoryCacheCurrencyGetter(CurrencyGetter):
 
     @staticmethod
     def create_memory() -> MemoryCurrencyGetter:
-        return MemoryCurrencyGetter({})
+        return MemoryCurrencyGetter({}, defaultdict(asyncio.Lock))
 
     async def get_price(self, currency: str) -> float:
-        if currency not in self._memory.price:
-            await self._add_to_cache(currency)
-        if (
-            self._memory.price[currency].time_exp_cache
-            < await self._clock.get_current_time()
-        ):
-            await self._add_to_cache(currency)
-        return self._memory.price[currency].price
+        async with self._memory.locks[currency]:
+            if currency not in self._memory.price:
+                await self._add_to_cache(currency)
+            if (
+                self._memory.price[currency].time_exp_cache
+                < await self._clock.get_current_time()
+            ):
+                await self._add_to_cache(currency)
+            return self._memory.price[currency].price
 
     async def _add_to_cache(self, currency: str) -> None:
         self._memory.price[currency] = CachedCurrencyPrice(
--- tests/test_adapters/test_cache/test_currency_getter.py
+++ tests/test_adapters/test_cache/test_currency_getter.py
@@ -1,3 +1,6 @@
+import asyncio
+from datetime import datetime, timezone, timedelta
+
 import pytest
 
 from app.adapters.currency_getter import MemoryCacheCurrencyGetter
@@ -46,3 +49,44 @@ async def test_get_price_caching_many(target) -> None:
     await getter.get_price("CAD")
     await getter.get_price("EUR")
     assert counter.count_price == 2
+
+
+class SlowCounter(CurrencyGetter):
+    def __init__(self) -> None:
+        self.count_price = 0
+
+    async def get_price(self, currency: str) -> float:
+        self.count_price += 1
+        await asyncio.sleep(0.25)
+        return MOCK_PRICE
+
+
+async def test_get_price_slow_service() -> None:
+    slow_counter = SlowCounter()
+    getter = MemoryCacheCurrencyGetter(
+        slow_counter, MemoryCacheCurrencyGetter.create_memory(), UTCClock()
+    )
+
+    await asyncio.gather(
+        getter.get_price("EUR"),
+        getter.get_price("EUR"),
+        getter.get_price("CNY"),
+        getter.get_price("EUR"),
+    )
+    assert slow_counter.count_price == 2
+
+
+async def test_get_price_slow_service_speed() -> None:
+    slow_counter = SlowCounter()
+    getter = MemoryCacheCurrencyGetter(
+        slow_counter, MemoryCacheCurrencyGetter.create_memory(), UTCClock()
+    )
+
+    time = datetime.now(timezone.utc)
+    await asyncio.gather(
+        getter.get_price("USD"),
+        getter.get_price("CNY"),
+        getter.get_price("RUB"),
+        getter.get_price("EUR"),
+    )
+    assert time + timedelta(seconds=0.5) > datetime.now(timezone.utc)
